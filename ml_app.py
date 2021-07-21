34787aa97d7c5a2a1ca81f97a4c0eeb4bb4dc9ec
Agung Yoga Setiawan <78807215+AgungYogaSetiawan@users.noreply.github.com>
2021-07-21 09:01:53 +0800

    delete numpy

--- ml_app.py
+++ ml_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, confusion_matrix, classification_report
